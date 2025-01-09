88bc71c3281fb9506cfc24330fae785746a0cd3d
Iago Teles <iagooteles@gmail.com>
2025-01-09 19:14:02 -0300

    improved answer, now getting data from json file

--- desafio3.py
+++ desafio3.py
@@ -7,51 +7,20 @@
 # a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 # b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
-def analisar_faturamento(faturamento_diario):
-    dias_com_faturamento = [faturamento['valor'] for faturamento in faturamento_diario if faturamento['valor'] > 0]
+import json
 
+def analisar_faturamento(arquivo_json):
+    with open(arquivo_json, 'r') as arquivo:
+        faturamento_diario = json.load(arquivo)
+
+    dias_com_faturamento = [faturamento['valor'] for faturamento in faturamento_diario if faturamento['valor'] > 0]
     menor_valor = min(dias_com_faturamento)
     maior_valor = max(dias_com_faturamento)
-
     media = sum(dias_com_faturamento) / len(dias_com_faturamento)
-
     dias_acima_da_media = len([d for d in dias_com_faturamento if d > media])
 
     print(f"Menor valor de faturamento: R${menor_valor:.2f}")
     print(f"Maior valor de faturamento: R${maior_valor:.2f}")
     print(f"Número de dias com faturamento acima da média: {dias_acima_da_media}")
 
-faturamento_diario = [
-    {"dia": 1, "valor": 22174.1664},
-    {"dia": 2, "valor": 24537.6698},
-    {"dia": 3, "valor": 26139.6134},
-    {"dia": 4, "valor": 0.0},
-    {"dia": 5, "valor": 0.0},
-    {"dia": 6, "valor": 26742.6612},
-    {"dia": 7, "valor": 0.0},
-    {"dia": 8, "valor": 42889.2258},
-    {"dia": 9, "valor": 46251.174},
-    {"dia": 10, "valor": 11191.4722},
-    {"dia": 11, "valor": 0.0},
-    {"dia": 12, "valor": 0.0},
-    {"dia": 13, "valor": 3847.4823},
-    {"dia": 14, "valor": 373.7838},
-    {"dia": 15, "valor": 2659.7563},
-    {"dia": 16, "valor": 48924.2448},
-    {"dia": 17, "valor": 18419.2614},
-    {"dia": 18, "valor": 0.0},
-    {"dia": 19, "valor": 0.0},
-    {"dia": 20, "valor": 35240.1826},
-    {"dia": 21, "valor": 43829.1667},
-    {"dia": 22, "valor": 18235.6852},
-    {"dia": 23, "valor": 4355.0662},
-    {"dia": 24, "valor": 13327.1025},
-    {"dia": 25, "valor": 0.0},
-    {"dia": 26, "valor": 0.0},
-    {"dia": 27, "valor": 25681.8318},
-    {"dia": 28, "valor": 1718.1221},
-    {"dia": 29, "valor": 13220.495},
-    {"dia": 30, "valor": 8414.61}
-]
-
-analisar_faturamento(faturamento_diario)
+analisar_faturamento('dados.json')
